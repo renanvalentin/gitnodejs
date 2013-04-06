220162064db0837f28f9df486e64bce8da24ca1a
Michael Lancaster <mlancaster@golinharris.com>
2013-04-06 10:51:56 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+//Node function called each time a new HTTPrequest arrives
+function onRequest(req,res) {
+	res.writeHead(200,{'Content-Type':'text/html'});
+	res.end('Hello Michael!');
+	/*WritetheIPaddressesofourconnectingclienttoconsole*/
+}
+//Listen for connectionson the port provided by the host process
+var server=http.createServer(onRequest).listen(process.env.PORT || 8080);
