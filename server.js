552fa8ba7bffd63c3694eb5209bedf44229a64d9
Michael Lancaster <mlancaster@golinharris.com>
2013-04-06 11:13:19 -0500

    change back nodejs

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var http = require('http');
 //Node function called each time a new HTTPrequest arrives
 function onRequest(req,res) {
-	res.writeHead(200,{'Content-Type':'text/plain'});
-	res.end('Hello '+ req.connection.remoteAddress +'!');
-	/*WritetheIPaddressesofourconnectingclienttoconsole*/
-	console.log('Incoming connection from '+req.connection.remoteAddress);
+	res.writeHead(200,{'Content-Type':'text/html'});
+	res.end('Hello Michael!');
 }
 //Listen for connectionson the port provided by the host process
 var server=http.createServer(onRequest).listen(process.env.PORT || 8080);
